2a6689c37c90c7887348d3279b8298cf71d18d43
Adam Bergman <adam@fransvilhelm.com>
2019-07-04 15:00:33 +0200

    chore: Setup dev env

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,16 @@
+/* eslint-disable global-require */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
-  plugins: [require('autoprefixer')],
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('postcss-preset-env')({ stage: 3 }),
+    isProd &&
+      require('@fullhuman/postcss-purgecss')({
+        content: ['**/*.html', 'assets/**/*.ts'],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+      }),
+  ].filter(Boolean),
 };
